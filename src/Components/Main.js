9dcc48db7fd86b08f3d1c92560276862a00a0d7b
AndrejaKardos <andreja.kardos@gmail.com>
2020-11-30 16:54:51 +1100

    Rearranged item components with flex display

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,12 +3,13 @@ import Header from './Header'
 import ItemLibrary from './ItemLibrary'
 import SelectedItems from './SelectedItems'
 import Footer from './Footer'
+import './Main.css'
 
 function Main() {
   return (
     <div className='main'>
         <Header />
-        <div>
+        <div className='main__items'>
             <ItemLibrary />
             <SelectedItems />
         </div>
