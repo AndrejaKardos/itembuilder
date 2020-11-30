9b68f3ca18f540d838e68c8c8c3861afe5a0f8b6
AndrejaKardos <andreja.kardos@gmail.com>
2020-11-30 16:39:45 +1100

    Added footer component to main app

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+    return(
+        <div>CONTACT INFO | FAQ | POLICY | SOCIAL MEDIA</div>
+    );
+}
+
+export default Footer
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import ItemLibrary from './ItemLibrary'
+import Footer from './Footer'
 
 function Main() {
   return (
     <div className='main'>
         <Header />
         <ItemLibrary />
+        <Footer />
     </div>
   )
 }
