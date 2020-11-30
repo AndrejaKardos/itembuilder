a10f9eaa1f163090f92ebacae58db7f55cb206ed
AndrejaKardos <andreja.kardos@gmail.com>
2020-11-30 16:57:36 +1100

    Added component for champion statistics

---
+++ src/Components/ChampionStats.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+function ChampionStats() {
+    return (
+        <div className='championstats'>STATS HERE</div>
+    );
+}
+
+export default ChampionStats
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import ItemLibrary from './ItemLibrary'
 import SelectedItems from './SelectedItems'
+import ChampionStats from './ChampionStats'
 import Footer from './Footer'
 import './Main.css'
 
@@ -12,6 +13,7 @@ function Main() {
         <div className='main__items'>
             <ItemLibrary />
             <SelectedItems />
+            <ChampionStats />
         </div>
         <Footer />
     </div>
