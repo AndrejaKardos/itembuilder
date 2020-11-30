11ed57cbbebf2a19c0f864c010ea029e457dbbe4
AndrejaKardos <andreja.kardos@gmail.com>
2020-11-30 16:37:15 +1100

    Added header component to main app

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='header'>
+            <div>LOGO | NAV BAR | SEARCH</div>
+        </div>
+    );
+}
+
+export default Header
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from './Header'
 import ItemLibrary from './ItemLibrary'
 
 function Main() {
   return (
-    <div className="app">
-      <ItemLibrary />
+    <div className='main'>
+        <Header />
+        <ItemLibrary />
     </div>
   )
 }
