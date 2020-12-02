9743dbde9d5070d17abdd7576353c660e9425af6
AndrejaKardos <andreja.kardos@gmail.com>
2020-12-02 18:18:02 +1100

    Added Runes component for runepages

--- src/Components/ChampionEquip.js
+++ src/Components/ChampionEquip.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SelectedItems from './SelectedItems'
 import ChampionStats from './ChampionStats'
+import Runes from './Runes'
 
 function ChampionEquip() {
     return(
         <div className='championeqip'>
             <SelectedItems />
             <ChampionStats />
+            <Runes />
         </div>
     );
 }
---
+++ src/Components/Runes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Runes() {
+    return (
+        <div className='runes'>RUNEPAGE HERE</div>
+    );
+}
+
+export default Runes
