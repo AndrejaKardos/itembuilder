21fe3a77853bcb8d3bc2f91bd72b23464da18b75
AndrejaKardos <andreja.kardos@gmail.com>
2020-12-08 16:54:43 +1100

    Included items in library

--- src/Components/ItemLibrary.js
+++ src/Components/ItemLibrary.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Searchbar from './Searchbar'
 import Filter from './Filter.js'
 import './ItemLibrary.css'
+import items from '../data/10.24.1/data/en_AU/item.json'
+
 
 function ItemLibrary() {
+    const data = items.data
+    Object.keys(data).map(key => {
+        const item = data[key]
+    })
+
+    console.log(Object.keys(data).map(id => 
+        data[id].name    
+    ))
+
     return (
         <div className="itemlibrary">
             <Searchbar />
