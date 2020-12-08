6224e48b099c6f04328ab6156e449a9cae0ed1d2
AndrejaKardos <andreja.kardos@gmail.com>
2020-12-08 14:45:35 +1100

    Added styling to Searchbar component

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import './Searchbar.css'
 
 
 function Searchbar() {
     return (
         <div className='searchbar'>
             <FontAwesomeIcon 
-                icon={faSearch} 
-                transform='shrink-1' 
-                className='searchbar__icon' 
-            />
+                    icon={faSearch} 
+                    transform='shrink-3' 
+                    className='searchbar__icon' 
+                />
+
+            <input type='search' className='searchbar__search'></input>
         </div>
     );
 }
