066001ad4cc08b6e3dbbb8ea38a56c56693ad8ab
AndrejaKardos <andreja.kardos@gmail.com>
2020-12-17 16:13:22 +1100

    Displayed item images in item library

--- src/Components/ItemLibrary.js
+++ src/Components/ItemLibrary.js
@@ -9,7 +9,6 @@ function ItemLibrary() {
     const itemUrl = "http://ddragon.leagueoflegends.com/cdn/10.25.1/img/item/"
     const itemId = Object.keys(items.data).map(key => itemUrl + items.data[key].image.full)
     console.log(itemId)
-    
 
     return (
         <div className="itemlibrary">
@@ -18,8 +17,13 @@ function ItemLibrary() {
             <div>EPIC</div>
             <div>LEGENDARY</div>
             <div>MYTHIC</div>
+            <div>
+                {itemId.map((el, i) => {
+                    return <img src={el} alt="Item image" key={i}></img>
+                })}
 
-            
+                <img src="http://ddragon.leagueoflegends.com/cdn/10.25.1/img/item/1001.png" />
+            </div>
         </div>
     )
 }
